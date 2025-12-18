cae3fe210b4fb89edbbb83f24ef6d705203dcdfc
Your Name <you@example.com>
2025-12-19 02:47:27 +0300

    step2

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,26 @@ class MyApp extends StatelessWidget {
           title: Text("Layouts", style: TextStyle(color: Colors.white)),
 
           actions: [Icon(Icons.cloud, color: Colors.white)],
-         
-         
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+              
+                children: [
+                     Image.asset("images/1.png", fit: BoxFit.contain),
+                     Positioned(
+                     bottom: 20,
+                     left: 80,
+                      child:Text("7/12/2021",style: TextStyle(fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.amberAccent),)),
+
+                ],
+              )],
+              
+          ),
+
         ),
       ),
     );
