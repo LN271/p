7c0568df2e108130cce9fefbb70b78c59a81d6a2
Your Name <you@example.com>
2025-12-19 03:10:30 +0300

    step4

--- lib/main.dart
+++ lib/main.dart
@@ -24,24 +24,49 @@ class MyApp extends StatelessWidget {
         ),
         body: SingleChildScrollView(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Stack(
-              
-                children: [
-                     Image.asset("images/1.png", fit: BoxFit.contain),
-                     Positioned(
-                     bottom: 20,
-                     left: 80,
-                      child:Text("7/12/2021",style: TextStyle(fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.amberAccent),)),
+              Container(
+                child: Stack(
+                  children: [
+                    Image.asset("images/1.png", fit: BoxFit.contain),
+                    Positioned(
+                      bottom: 20,
+                      left: 80,
+                      child: Text(
+                        "7/12/2021",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.amberAccent,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
 
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "My Birthday",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Container(child: Divider(height: 2)),
+                  Container(
+                    child: Text(
+                      "its going to be great Birthday, we are going out for \ndinner at my favonty place,them watch a moving after we \ngo to the gristeria for ice cream and espresso ",
+                    ),
+                  ),
+                  Container(child: Divider(height: 2)),
+                
 
                 ],
-              )],
-              
+              ),
+            ],
           ),
-
         ),
       ),
     );
